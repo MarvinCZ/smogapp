0baaf23fd95024b2555a51b1c7ff41cdec459520
Martin Brom <zed97@seznam.cz>
2016-09-24 19:49:32 +0200

    add state to data, now works on all states

--- js/parser.js
+++ js/parser.js
@@ -3,25 +3,28 @@ function getData(){
     var long = "";
     var data = [];
     var measurements = {};
-    var cz = json['States'][0];
     var components;
     var index = 0;
     var name = '';
-    for(var i=0;i<cz['Regions'].length;i++) {
-        for(var j=0;j<cz['Regions'][i]['Stations'].length;j++) {
-            index = cz['Regions'][i]['Stations'][j]['Ix'];
-            name = cz['Regions'][i]['Stations'][j]['Name'];
-            if(cz['Regions'][i]['Stations'][j]['Code'] != ""){
-                lat = cz['Regions'][i]['Stations'][j]['Lat'];
-                long = cz['Regions'][i]['Stations'][j]['Lon'];
-                components = cz['Regions'][i]['Stations'][j]['Components'];
-                measurements = {};
-                for(var k=0;k<components.length;k++) {
-                    if(components[k]['Ix'] > 0 || components[k]['Ix'] < -1) {
-                        measurements[components[k]['Code']] = [components[k]['Val']];
+    var state = '';
+    for(var h=0;h<json['States'].length;h++) {
+        state = json['States'][h]['Code'];
+        for(var i=0;i<json['States'][h]['Regions'].length;i++) {
+            for(var j=0;j<json['States'][h]['Regions'][i]['Stations'].length;j++) {
+                index = json['States'][h]['Regions'][i]['Stations'][j]['Ix'];
+                name = json['States'][h]['Regions'][i]['Stations'][j]['Name'];
+                if(json['States'][h]['Regions'][i]['Stations'][j]['Code'] != ""){
+                    lat = json['States'][h]['Regions'][i]['Stations'][j]['Lat'];
+                    long = json['States'][h]['Regions'][i]['Stations'][j]['Lon'];
+                    components = json['States'][h]['Regions'][i]['Stations'][j]['Components'];
+                    measurements = {};
+                    for(var k=0;k<components.length;k++) {
+                        if(components[k]['Ix'] > 0 || components[k]['Ix'] < -1) {
+                            measurements[components[k]['Code']] = [components[k]['Val']];
+                        }
                     }
+                    data.push({state: state, name: name, index: index, lat: lat, long: long, data: measurements});
                 }
-                data.push({name: name, index: index, lat: lat, long: long, data: measurements});
             }
         }
     }
